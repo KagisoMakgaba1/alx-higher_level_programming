b356b6610785ee9ad45a50f23e6e94ce11d75126
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-01-02 08:49:57 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-for tens_digit in range(10):
-    for units_digit in range(tens_digit + 1, 10):
-        print("{:02}, ".format(tens_digit * 10 + units_digit), end="" if units_digit < 9 else "\n")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
